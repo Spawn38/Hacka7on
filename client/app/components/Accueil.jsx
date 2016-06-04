4896d9a1149116e305f0031cc026d84ddf207a6f
Damien <damien.jacinto@gmail.com>
2016-06-04 16:09:13 +0200

    dernier modif v2

--- client/app/components/Accueil.jsx
+++ client/app/components/Accueil.jsx
@@ -11,8 +11,8 @@ export default class Accueil extends Component {
 
 	render() {
 		return (
-			<div className="mainExterieur">
-				<img src="http://res.cloudinary.com/spawn/image/upload/v1464993046/MyLussi_sicexv.png"/>
+			<div>
+				<img src="http://res.cloudinary.com/spawn/image/upload/c_crop,w_566/v1464993046/MyLussi_sicexv.png"/>
 			</div>
 		);
 	}
